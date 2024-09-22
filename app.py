4ca7b083da46b1e82856ec3489af66b56b165894
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-09-21 19:55:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ def generar_ecuacion():
     # Generar coeficientes aleatorios para la ecuación ax + b = 0
     a = random.randint(1, 10)
     b = random.randint(-10, 10)
-    c = random.randint(-10, 20)
-    return a, b, c
+    return a, b
 
 # Función para resolver la ecuación ax + b = 0
-def resolver_ecuacion(a, b, c):
+def resolver_ecuacion(a, b):
     if a != 0:
-        return (c-b)/a
+        return b/a
     else:
         return None
 
@@ -22,12 +21,17 @@ st.title("Generador y Solucionador de Ecuaciones de 1º")
 # Botón para generar una nueva ecuación
 if st.button("Generar Ecuación: Ax+B=C"):
     a, b = generar_ecuacion()
-    st.write(f"La ecuación generada es: {a}x + {b} = {c}")
+    st.write(f"La ecuación generada es: {a}x + {b} = 0")
     # Resolver la ecuación
     # solucion = resolver_ecuacion(a, b, c)
     #if solucion is not None:
         #st.write(f"La solución es: x = {solucion}")
     #else:
         #st.write("La ecuación no tiene solución, ya que a = 0.")
+
+
+
+
+
 
 
