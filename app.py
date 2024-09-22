45f90a7f077917d8a540b86618a8cbd196b76e85
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2024-09-21 19:49:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ def generar_ecuacion():
     # Generar coeficientes aleatorios para la ecuación ax + b = 0
     a = random.randint(1, 10)
     b = random.randint(-10, 10)
-    return a, b
+    c = random.randint(-10, 20)
+    return a, b, c
 
 # Función para resolver la ecuación ax + b = 0
-def resolver_ecuacion(a, b):
+def resolver_ecuacion(a, b, c):
     if a != 0:
-        return -b / a
+        return (c-b)/a
     else:
         return None
 
@@ -19,10 +20,17 @@ def resolver_ecuacion(a, b):
 st.title("Generador y Solucionador de Ecuaciones de 1º")
 
 # Botón para generar una nueva ecuación
-if st.button("Generar Ecuación: Ax+B=0"):
+if st.button("Generar Ecuación: Ax+B=C"):
     a, b = generar_ecuacion()
-    st.write(f"La ecuación generada es: {a}x + {b} = 0")
+    st.write(f"La ecuación generada es: {a}x + {b} = {c}")
+    # Resolver la ecuación
+    solucion = resolver_ecuacion(a, b, c)
+    if solucion is not None:
+        st.write(f"La solución es: x = {solucion}")
+    else:
+        st.write("La ecuación no tiene solución, ya que a = 0.")
 
+"""
 solucion1 = resolver_ecuacion(a, b)
 # Botón para generar una nueva ecuación
 if st.button(f"Solución de: {a}x + {b} = 0"):
@@ -30,3 +38,4 @@ if st.button(f"Solución de: {a}x + {b} = 0"):
         st.write(f"La solución es: x = {solucion1}")
     else:
         st.write("La ecuación no tiene solución, ya que a = 0.")
+"""
